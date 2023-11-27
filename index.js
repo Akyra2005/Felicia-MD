5ea7d53315c3eedd2ac5dc2781c0b6e66cdf6e74
Qalesya Akyra Rukma Quinnsha <94896069+Akyra2005@users.noreply.github.com>
2023-11-27 23:37:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -59,23 +59,16 @@ async function start(file) {
   });
 
   p.on('exit', (code) => {
-  isRunning = false;
-  console.error(chalk.red(`🛑 Exited with code: ${code}`));
+    isRunning = false;
+    console.error(chalk.red(`Exited with code: ${code}`));
 
-  if (code === null && // Aplikasi mengalami crash
-    !isCrashedBySignal()) // Aplikasi tidak mengalami crash karena signal
-    start.apply(this, arguments);
-});
-
-function isCrashedBySignal() {
-  // Cek apakah aplikasi mengalami crash karena signal
-  return code === signal;
-}
+    if (code === 0) return;
 
     fs.watchFile(args[0], () => {
       fs.unwatchFile(args[0]);
       start('main.js');
     });
+  });
 
   p.on('error', (err) => {
     console.error(chalk.red(`❌ Error: ${err}`));
